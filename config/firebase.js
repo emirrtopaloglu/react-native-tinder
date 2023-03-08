95d543af5c41a6f3b2798f812297c263aaaed430
emirrtopaloglu <emirrttopaloglu@gmail.com>
2023-03-08 23:17:52 +0300

    added firebase to project

---
+++ config/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmMHY_zygWXrMmyfP1KWDe1mev4TnPKGk",
+  authDomain: "react-native-tinder-a94f2.firebaseapp.com",
+  projectId: "react-native-tinder-a94f2",
+  storageBucket: "react-native-tinder-a94f2.appspot.com",
+  messagingSenderId: "648403455597",
+  appId: "1:648403455597:web:cd18cd5a6cdc2b15607964",
+  measurementId: "G-6VX2D2W1WC",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
